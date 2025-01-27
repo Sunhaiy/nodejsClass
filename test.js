e6abd9de4fce72156e6fdff931986a0868402865
孫海洋 <s744129991@outlook.com>
2025-01-27 18:22:10 +0800

    完成基本访问功能，目前还有登录有验证码没完成

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
-const puptt =require('puppeteer')
-const start =async()=>{
-     console.log('hello')
-    const browser =await puptt.launch({headless:false})
-    const page =await browser.newPage()
-    
-    await page.goto("https://www.zhihuishu.com/")
-    
-    await page.screenshot({path:'example.png'})
-}
-start()
+const puptt = require('puppeteer');
+const start = async () => {
+    console.log('hello');
+    const browser = await puptt.launch({ headless: false });
+    const page = await browser.newPage();
+    page.setViewport({ width: 1920, height: 1080 });
+    await page.goto("https://www.zhihuishu.com/");
+    await page.waitForSelector('#notLogin');
+    page.click('#notLogin');
+
+    await page.waitForSelector('#lUsername');
+    await page.waitForSelector('#lPassword');
+    await page.type('#lUsername', '###');
+    await page.type('#lPassword', '###');
+
+    await setTimeout(() => {
+        
+    }, 3000);
+    console.log('hello');
+    await page.waitForSelector('.wall-sub-btn', { visible: true });
+    await page.click('span.wall-sub-btn');
+};
+
+start();
